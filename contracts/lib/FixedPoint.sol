a06b34b5ddd10f2ebf7a2f5e8bd79412083043fb
jongregis <jon.gregis@gmail.com>
2023-01-30 16:48:18 -0600

    add oracle tests

--- contracts/lib/FixedPoint.sol
+++ contracts/lib/FixedPoint.sol
@@ -8,8 +8,6 @@ library FixedPoint {
         uint224 _x;
     }
 
-    // range: [0, 2**144 - 1]
-    // resolution: 1 / 2**112
     struct uq144x112 {
         uint256 _x;
     }
@@ -19,13 +17,10 @@ library FixedPoint {
         return uq112x112((uint224(numerator) << RESOLUTION) / denominator);
     }
 
-    // decode a UQ144x112 into a uint144 by truncating after the radix point
     function decode144(uq144x112 memory self) internal pure returns (uint144) {
         return uint144(self._x >> RESOLUTION);
     }
 
-    // multiply a UQ112x112 by a uint, returning a UQ144x112
-    // reverts on overflow
     function mul(uq112x112 memory self, uint256 y) internal pure returns (uq144x112 memory) {
         uint256 z = 0;
         require(y == 0 || (z = self._x * y) / y == self._x, "FixedPoint: MUL_OVERFLOW");
--- test/UniswapV2TWAPOracle.t.sol
+++ test/UniswapV2TWAPOracle.t.sol
@@ -14,23 +14,60 @@ contract UniswapV2TWAPOracleTest is Test {
     uint256 granularity = 2;
     uint256 mainnetFork;
 
+    event JobDeleted(uint256 indexed jobID);
+    event JobCreated(uint256 indexed jobID);
+    event JobExecuted(uint256 indexed jobID, uint256 timestamp);
+
+    error PairNotFound(address token0, address token1);
+
     function setUp() public {
         mainnetFork = vm.createSelectFork(vm.rpcUrl("mainnet"));
-        token0 = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-        token1 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+        token0 = 0x6B175474E89094C44Da98b954EedeAC495271d0F; // DAI
+        token1 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; // WETH
         uniswapV2Factory = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
         admin = makeAddr("admin");
     }
 
-    function testCreateJob(uint104 _granularity) public {
+    function forkFixture() public {
+        vm.selectFork(mainnetFork);
+        vm.rollFork(16_461_853);
+        vm.startPrank(admin);
+        oracle = new UniswapV2TWAPOracle();
+        vm.label(address(oracle), "oracle");
+        oracle.initialize(uniswapV2Factory, admin);
+        oracle.createJob(token1, token0, periodSize, granularity);
+    }
+
+    function test_create_CreatesJob(uint104 _granularity) public {
         vm.assume(_granularity > 1);
         vm.startPrank(admin);
         oracle = new UniswapV2TWAPOracle();
         oracle.initialize(uniswapV2Factory, admin);
+
+        vm.expectEmit(true, true, true, true);
+        emit JobCreated(1);
         oracle.createJob(token1, token0, periodSize, _granularity);
     }
 
-    function testUpdateJob(uint104 _granularity, uint104 _periodSize) public {
+    function testRevert_create_CreateJobFail() public {
+        vm.startPrank(admin);
+        oracle = new UniswapV2TWAPOracle();
+        oracle.initialize(uniswapV2Factory, admin);
+
+        vm.expectRevert();
+        oracle.createJob(token1, token0, periodSize, 1);
+    }
+
+    function testRevert_create_PairNotFound() public {
+        vm.startPrank(admin);
+        oracle = new UniswapV2TWAPOracle();
+        oracle.initialize(uniswapV2Factory, admin);
+
+        vm.expectRevert(abi.encodeWithSelector(PairNotFound.selector, token1, token1));
+        oracle.createJob(token1, token1, periodSize, 2);
+    }
+
+    function test_update_UpdatesJob(uint104 _granularity, uint104 _periodSize) public {
         vm.assume(_granularity > 1);
         vm.assume(_periodSize > 0);
         vm.startPrank(admin);
@@ -40,21 +77,53 @@ contract UniswapV2TWAPOracleTest is Test {
         oracle.updateJob(1, _periodSize, _granularity);
     }
 
-    function testDeleteJob() public {
+    function test_delete_DeletesJob() public {
         vm.startPrank(admin);
         oracle = new UniswapV2TWAPOracle();
         oracle.initialize(uniswapV2Factory, admin);
         oracle.createJob(token1, token0, periodSize, granularity);
+
+        vm.expectEmit(true, true, true, true);
+        emit JobDeleted(1);
         oracle.deleteJob(1);
         assertEq(oracle.getActiveJobIDs().length, 0);
     }
 
-    function testForkQueryData() public {
-        vm.selectFork(mainnetFork);
+    function testRevert_delete_DeleteNonExistantJob() public {
         vm.startPrank(admin);
         oracle = new UniswapV2TWAPOracle();
         oracle.initialize(uniswapV2Factory, admin);
         oracle.createJob(token1, token0, periodSize, granularity);
-        // oracle.performUpkeep(abi.encode(1));
+
+        vm.expectRevert();
+        oracle.deleteJob(2);
+    }
+
+    function testRevert_queryPrice_QueryPriceWithZeroValue() public {
+        vm.startPrank(admin);
+        oracle = new UniswapV2TWAPOracle();
+        oracle.initialize(uniswapV2Factory, admin);
+
+        vm.expectRevert();
+        oracle.queryPrice(1, token1, 0, token0);
+    }
+
+    function testFork_queryPrice_QuerysData() public {
+        forkFixture();
+        vm.warp(block.timestamp + 100);
+        oracle.performUpkeep(abi.encode(1));
+
+        vm.warp(block.timestamp + 100);
+        oracle.performUpkeep(abi.encode(1));
+        oracle.queryPrice(1, token1, 1 ether, token0);
+    }
+
+    function testFork_performUpkeep_RunUpkeep() public {
+        forkFixture();
+        vm.warp(block.timestamp + 100);
+
+        vm.expectEmit(true, true, true, true);
+        emit JobExecuted(1, block.timestamp);
+        oracle.performUpkeep(abi.encode(1));
     }
 }
